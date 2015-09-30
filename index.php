398f3547d1e129441461323abac8e5c380f5c843
brubenstein <brubenstein@calstrawberry.org>
2015-09-30 11:10:51 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Azure Test</title>
+	</head>
+	<body>
+		<h1>Hello, World!</h1>
+		<?php echo "PHP Hello World"; ?>
+	</body>
+</html>
